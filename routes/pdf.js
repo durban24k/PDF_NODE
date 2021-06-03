a6bd1f7044773a70844a9a438c3644b420d18fda
Wanjau Alfred Mambo <56720300+durban24k@users.noreply.github.com>
2021-06-03 18:10:43 +0300

    v1.0.0

--- routes/pdf.js
+++ routes/pdf.js
@@ -5,22 +5,34 @@ const Router=express.Router();
 
 Router.post('/pdf',(req,res)=>{
      // instantiate the library
-     let doc = new PDFGenerator();
+     // let doc = new PDFGenerator();
 
-     let {fileNo}=req.body;
-     // console.log(fileNo);
+     // let {fileNo}=req.body;
+     // // console.log(fileNo);
 
-     let pdfDoc = fs.createWriteStream(String(fileNo))
-     // pipe to a writable stream which would save the result into the same directory
-     doc.pipe(pdfDoc);
+     // let pdfDoc = fs.createWriteStream(String(fileNo))
+     // // pipe to a writable stream which would save the result into the same directory
+     // doc.pipe(pdfDoc);
 
-     doc.text('Some awesome example text', { bold: true,
-     underline: true,
-     align: 'center'
+     // doc.text('Some awesome example text', { bold: true,
+     // underline: true,
+     // align: 'center'
+     // })
+     // // write out file
+     // doc.end();
+
+     let {
+          id,
+          fileNo
+     } = req.body;
+     let time= new Date();
+     let data=`\n${id} submitted ${fileNo} at ${time}`;
+     fs.appendFile('logs/SessionLog.txt',data,(err)=>{
+          if (err) throw err;
      })
-     // write out file
-     doc.end();
+
      res.redirect('/');
+
      
 });
 module.exports=Router;
